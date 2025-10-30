131ec256bdccf769426df76e669c209462ef5f9d
Alejo1708 <alejo20081701@gmail.com>
2025-10-29 19:53:35 -0500

    Este es el codigo 2 del parcial 2

---
+++ src/Codigo2.java
@@ -0,0 +1,6 @@
+public class Codigo2 {
+    public static void main(String[] args) {
+        Jugador jugador1 = new Jugador("Alejandro Betancur", "17010878A", "17/01/2008", 98335);
+        jugador1.mostrarInfo();
+    }
+}
